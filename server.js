12f74eeb7edd62abd2562ba8213a0b6a55e62dd4
Synui <jonathon.schoenholz@yahoo.com>
2022-01-27 16:06:48 -0500

    Uodated Vote model

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
-// chaning force: fasle to true is the same as DROP TABLE IF EXISTS in MYSQL
-sequelize.sync({ force: false}).then(() => {
+// changing force: fasle to true is the same as DROP TABLE IF EXISTS in MYSQL
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
