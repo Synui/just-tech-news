a13f0c90f567e16747a159f5494164fc85921987
Synui <jonathon.schoenholz@yahoo.com>
2022-02-01 15:09:05 -0500

    Homepage added

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+// access to stylesheet within express app
+const path = require('path');
+
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -7,6 +10,16 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// must be above routes
+app.use(express.static(path.join(__dirname, 'public')));
+
+// defining app template engine using handlebards
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
